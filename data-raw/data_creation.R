7a262c6b58a8dc61181d2939441011f9d0f3cf2b
Lucie Contamin <contamin.lucie@gmail.com>
2018-11-05 11:18:40 +0700

    Create sysdata and raw_data folder

---
+++ data-raw/data_creation.R
@@ -0,0 +1,13 @@
+# Save internal data:
+# Save Vietnam admin1 administrative boundarie map from 2004 to 2007, use in the
+# function hist_gadm to be able to recreate the merge event of `Ha Tay` and
+# `Ha Noi` in 2008.
+vn_a1_0407 <- readRDS("data-raw/gadm_vn_0407.rds")
+
+# Save data and empty environment ----------------------------------------------
+
+devtools::use_data(vn_a1_0407,
+                   internal = TRUE, overwrite = TRUE)
+
+rm(list = ls())
+
