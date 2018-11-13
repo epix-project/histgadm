b59acc7bb70f17019034999b758e8bc44cf6039a
Lucie Contamin <contamin.lucie@gmail.com>
2018-11-13 10:21:13 +0700

    Separate functions in multiple .R files

---
+++ R/map_data.R
@@ -0,0 +1,55 @@
+# ------------------------------------------------------------------------------
+#' Creates a list of historical map in a package
+#'
+#' Creates a list of historical map with the function
+#' \code{\link[histgadm]{hist_map}} and stores the download GADM
+#' (\url{https://gadm.org}) files in a folder data_raw and the output maps in
+#' the folder data of a package.
+#'
+#' @param path character string path of the package.
+#' @param country character string, name of the country to download.
+#' @param hash named character vector containing the translation in English
+#'  (standardized version) of the admin1 names. See `Details` for more
+#'  information.
+#' @param lst_history A list containing a list of event, each code with a slot
+#'  after, a slot before, a slotevent (split/merge/rename/ complexe merge/
+#'  complexe split) and a slot year. See `Details` for more information.
+#' @param from Initial date of the time range selected, of the class Date,
+#'   character or numeric. By default "1960".
+#' @param to Final date of the time range selected, of the class Date, character
+#'  or numeric, by default "2020".
+#' @param d.hash used in case of `complexe split` or `complexe merge` in the
+#'  `lst_history` object.  named character vector containing the translation in
+#'  English (standardized version) of the admin2 names. See `Details` for more
+#'  information.
+#' @param tolerance numeric for thinning (simplification). the tolerance value
+#'  should be in the metric of the input object (cf. from function
+#'  \code{\link[maptools]{thinnedSpatialPoly}}). By default, tolerance = 0.01.
+#'
+#' @examples
+#'
+#' library(dictionary)
+#' \dontrun{
+#' map_data("PACKAGE", "Cambodia", kh_province, kh_history)
+#' }
+#'
+#' @importFrom devtools use_data_raw
+#' @importFrom eply evals
+#' @importFrom usethis use_data
+#'
+#' @export
+map_data <- function(path, country, hash, lst_history, from = "1960",
+                     to = "2020", d.hash = NULL, tolerance = 0.01) {
+  setwd(path)
+  datarawdir <- paste0(path, "/data-raw")
+  if (!dir.exists(datarawdir)) usethis::use_data_raw()
+  datadir <- paste0(path, "/data")
+  if (!dir.exists(datadir)) dir.create(datadir)
+  data <- hist_map(country = country, hash = hash, lst_history = lst_history,
+                   from = from, to = to, d.hash = d.hash,
+                   tolerance = tolerance, file_rm = FALSE, path = datarawdir)
+  list2env(data, envir = environment())
+  eply::evals(paste0("usethis::use_data(`", paste(names(data),
+                                                  collapse = "`, `"),
+                     "`, overwrite = TRUE)"))
+}
---
+++ R/map_documentation.R
@@ -0,0 +1,81 @@
+# ------------------------------------------------------------------------------
+#' Format documentation of a data.frame
+#'
+#' From a data frame, return one vector of length one containing:
+#' An object of class \code{data.frame, sf} with `THE DIMENSION OF THE DF`.
+#' And a list of the columns name and the class of each column.
+#'
+#' @param df a object of class `data.frame`.
+#'
+#' @importFrom roxygen2 object_format
+#' @keywords internal
+#' @noRd
+make_format <- function(df){
+  paste0(roxygen2::object_format(df), "\n \\itemize{",
+         lapply(seq_len(dim(df)[2]), function(x) {
+           paste0("\\item \\code{", names(df)[x], "} ",
+                  paste0("A column of class : ",
+                         class(df[[x]]) %>% paste(collapse = ", "), "."))
+         }) %>%
+           paste0(collapse = "\n"), "}")
+}
+
+# ------------------------------------------------------------------------------
+#' Makes documentation for sf map in a package
+#'
+#' Create a Rd file for the data, containing the documentation for each sf
+#' object in the package.
+#'
+#' @param path character string path of the package.
+#'
+#' @importFrom stringr str_extract
+#' @importFrom Rd2roxygen create_roxygen
+#' @importFrom utils capture.output
+#' @export
+map_documentation <- function(path) {
+
+  list_tab <- dir(paste0(path, "/data/"))
+  tot_rd <-  lapply(seq_along(list_tab), function(x){
+
+    load(paste0(path, "/data/", list_tab[x]))
+    df <- get(gsub(".rda", "", list_tab[x]))
+    from <- list_tab[x] %>% gsub("[^[:digit:]]", "", .)  %>% substr(1, 4)
+    to <- list_tab[x] %>% gsub("[^[:digit:]]", "", .)  %>% substr(5, 8)
+    quality <- list_tab[x] %>% stringr::str_extract("high|low")
+    country <- list_tab[x] %>% substr(1, 2) %>%
+      countrycode::countrycode("iso2c", "country.name")
+    source <- paste0("GADM (version ",
+                     dir(paste0(path, "/data-raw/")) %>% substr(5, 6) %>%
+                       unique %>% strsplit("") %>% unlist %>%
+                       paste(collapse = "."),
+                     ") data base from \\url{www.gadm.org}")
+
+    if (grep("[[:digit:]]", list_tab[x]) %>% length > 0) {
+
+      doc <- list(
+        title = paste0("Admin1 Administrative boundaries of ", country,
+                       " from ", from, " to ", to, "."),
+        format = make_format(df),
+        desc = paste0(
+          "Maps of the admin1 administrative boundaries of ", country,
+          " expressed from ", from, " to ", to, " in ", quality, " quality."),
+        source = source)
+
+    } else {
+
+      doc <- list(
+        title = paste0(country, "Country boundaries"),
+        format = make_format(df),
+        desc = paste0(
+          "Maps of the country administrative boundaries of ", country,
+          " expressed in ", quality, "quality."),
+        source = source)
+    }
+    doc <- capture.output(cat(Rd2roxygen::create_roxygen(doc), sep = "\n"))
+    doc <- c(doc,
+             paste0("'", gsub(".rda", "", list_tab[x]) %>%
+                      as.character(), "'\n"))
+  })
+  writeLines(unlist(tot_rd), con = paste0("R/data.R"), sep = "\n")
+  roxygen2::roxygenize(path)
+}
