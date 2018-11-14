9a03d6de1569f5fd09d8dcb4fa2070a3cae038d3
Lucie Contamin <contamin.lucie@gmail.com>
2018-11-14 10:10:19 +0700

    Correct test for map_documentation function

--- tests/testthat/tests_map_documentation.R
+++ tests/testthat/tests_map_documentation.R
@@ -1,22 +1,28 @@
+
 library(dictionary) # for "XX_history", "XX_province"
 
 context("`map_documentation`")
 
-test_that("`map_documentation` returns the correct output", {
+testthat::test_that("`map_documentation` returns the correct output", {
 
-  setwd("~/OneDrive/Shared/histgadm")
+  setwd("~/OneDrive/Shared/histgadm/tests/testthat")
   path <- "~/OneDrive/Shared/histgadm/tests/testthat/pkgtest"
   map_data(path, "Cambodia", kh_province, kh_history)
+  setwd("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest")
   map_documentation(path)
 
   test1 <- dir("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/")
 
   testthat::expect_length(test1, 8)
+
   unlink("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/data/",
          recursive = TRUE)
   unlink("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/data-raw/",
          recursive = TRUE)
-  file.remove(dir("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/"))
-  file.remove(dir("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/R/"))
+  file.remove(paste("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/",
+                    dir("man/"), sep = ""))
+  file.remove(paste("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/",
+                    dir("R/"), sep = ""))
 
+  setwd("~/OneDrive/Shared/histgadm")
 })
