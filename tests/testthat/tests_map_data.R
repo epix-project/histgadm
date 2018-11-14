3d79b39cc936ac6a5ed3c6c88f82d58236b61c02
Lucie Contamin <contamin.lucie@gmail.com>
2018-11-14 17:14:55 +0700

    Update tests with a temporary package in a tempdir

--- tests/testthat/tests_map_data.R
+++ tests/testthat/tests_map_data.R
@@ -1,16 +1,17 @@
 library(dictionary) # for "XX_history", "XX_province"
+library(usethis)
 
 context("`map_data`")
 
 test_that("`map_data` returns the correct output", {
 
-  setwd("~/OneDrive/Shared/histgadm")
-  path <- "~/OneDrive/Shared/histgadm/tests/testthat/pkgtest"
-  map_data(path, "Cambodia", kh_province, kh_history)
-  test1 <- dir("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/data/")
+  tmp <- file.path(tempdir(), "pkgtest")
+  create_package(tmp, open = FALSE)
+  map_data(tmp, "Cambodia", kh_province, kh_history)
+
+  test1 <- dir(paste0(tmp, "/data/"))
 
   testthat::expect_length(test1, 8)
-  unlink("tests/testthat/pkgtest/data/", recursive = TRUE)
-  unlink("tests/testthat/pkgtest/data-raw/", recursive = TRUE)
+  unlink(tmp, recursive = TRUE)
 
 })
--- tests/testthat/tests_map_documentation.R
+++ tests/testthat/tests_map_documentation.R
@@ -1,27 +1,17 @@
 library(dictionary) # for "XX_history", "XX_province"
+library(usethis)
 
 context("`map_documentation`")
 
 testthat::test_that("`map_documentation` returns the correct output", {
 
-  setwd("~/OneDrive/Shared/histgadm/tests/testthat")
-  path <- "~/OneDrive/Shared/histgadm/tests/testthat/pkgtest"
-  map_data(path, "Cambodia", kh_province, kh_history)
-  setwd("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest")
-  map_documentation(path)
+  tmp <- file.path(tempdir(), "pkgtest")
+  create_package(tmp, open = FALSE)
+  map_data(tmp, "Cambodia", kh_province, kh_history)
+  map_documentation(tmp)
 
-  test1 <- dir("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/")
+  test1 <- dir(paste0(tmp, "/man/"))
 
   testthat::expect_length(test1, 8)
-
-  unlink("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/data/",
-         recursive = TRUE)
-  unlink("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/data-raw/",
-         recursive = TRUE)
-  file.remove(paste("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/",
-                    dir("man/"), sep = ""))
-  file.remove(paste("~/OneDrive/Shared/histgadm/tests/testthat/pkgtest/man/",
-                    dir("R/"), sep = ""))
-
-  setwd("~/OneDrive/Shared/histgadm")
+  unlink(tmp, recursive = TRUE)
 })
