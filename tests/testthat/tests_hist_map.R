c18ad185a6fea343b2e1612f4085a5400cd15682
Lucie Contamin <contamin.lucie@gmail.com>
2019-01-25 09:05:55 +0700

    Update tests

--- tests/testthat/tests_hist_map.R
+++ tests/testthat/tests_hist_map.R
@@ -7,18 +7,19 @@ context("`hist_map`")
 
 test_that("`hist_map` returns the correct output", {
 
-  test1a <- hist_map("Cambodia", kh_province, kh_history, intlib = FALSE)
+  test1a <- hist_map("Cambodia", kh_province, kh_history, intlib = FALSE,
+                     path = FALSE)
   testthat::expect_equal(length(test1a), 8)
 
   test1b <- hist_map("Cambodia", kh_province, kh_history, intlib = FALSE,
-                    lst_province_year = kh_province_year)
+                     path = FALSE, lst_province_year = kh_province_year)
   testthat::expect_equal(length(test1b), 8)
 
   test2 <- hist_map("Laos", la_province, la_history, d.hash = la_district,
-                    intlib = FALSE)
+                    path = FALSE, intlib = FALSE)
   testthat::expect_equal(length(test2), 8)
 
-  vn_08 <- hist_map("Vietnam", vn_province, vn_history,
+  vn_08 <- hist_map("Vietnam", vn_province, vn_history, path = FALSE,
                      from = 2008, to = 2008, intlib = FALSE)
   test3 <- vn_08 %>% purrr::map("province") %>% purrr::discard(is.null) %>%
     purrr::map(data.frame) %>%
@@ -26,7 +27,7 @@ test_that("`hist_map` returns the correct output", {
     unlist %>% unique
   testthat::expect_equal(test3, "2008-2020")
 
-  vn_8082 <- hist_map("Vietnam", vn_province, vn_history,
+  vn_8082 <- hist_map("Vietnam", vn_province, vn_history, path = FALSE,
                        from = 1980, to = 1982, intlib = FALSE)
   test4 <- vn_8082 %>%
     purrr::map("province") %>%
@@ -36,12 +37,12 @@ test_that("`hist_map` returns the correct output", {
     unlist %>% unique
   testthat::expect_equal(test4, "1979-1990")
 
-  test5 <- hist_map("France", intlib = FALSE)
+  test5 <- hist_map("France", intlib = FALSE, path = FALSE)
   testthat::expect_equal(length(test5), 4)
 
   test6 <- hist_map("Thailand", th_province, th_history, intlib = FALSE,
                     lst_province_year = th_province_year, from = "1960",
-                    to = "1980")
+                    to = "1980", path = FALSE)
   testthat::expect_equal(length(test6), 6)
 
 
